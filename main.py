5e47a32b80d06a3d9673bfb2eab4d4b058ba83f5
fu1fan <fyf.yf@qq.com>
2021-07-14 23:24:46 +0800

    初步测试开发环境

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
+from sdk import epd2in9_V2
 
 os.chdir(os.path.dirname(__file__))
 
 if __name__ == "__main__":
-    pass
+    epd = epd2in9_V2.EPD_2IN9_V2()
+    epd.init()
+    epd.Clear(0xFF)
+    epd.sleep()
+    epd.Dev_exit()
