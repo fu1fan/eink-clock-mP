c5a6bebe4a9bd05a78d4eaca261cd942f2b123e0
nbsunrui <2251118387@qq.com>
2021-07-16 11:50:08 +0800

    改进updater日志功能

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import os
+import traceback
+from sdk import logger
 from sdk import epd2in9_V2
 
 os.chdir(os.path.dirname(__file__))
 
 if __name__ == "__main__":
+    logger_main = logger.Logger(logger.Logger.DEBUG)
     epd = epd2in9_V2.EPD_2IN9_V2()
-    epd.init()
-    epd.clear(0xFF)
-    epd.sleep()
-    epd.exit()
+    try:
+        epd.init()
+        epd.clear(0xFF)
+    except:
+        logger_main.warn(traceback.format_exc)
+    else:
+        epd.sleep()
+        epd.exit()
