37b89e0679dd7bb18f3f50c22514c78df21f991a
nbsunrui <2251118387@qq.com>
2021-07-16 11:55:50 +0800

    改进logger

--- sdk/logger.py
+++ sdk/logger.py
@@ -51,7 +51,9 @@ class Logger:
             file = open(self.folder + self.name,
                         "a+", encoding="utf-8")
             if "\n" in text:
-                text = "\n" + text
+                text = "\n%s\n" % text
+            else:
+                text = text + "\n"
             content = "%s%s[%s]%s" % (
                 self.__levelDic[level], time.strftime("[%Y%m%d-%H:%M:%S]", time.localtime()), thename,
                 text)  # 格式[level][time][name]--event--
