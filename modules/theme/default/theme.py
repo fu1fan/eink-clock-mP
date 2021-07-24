5016efd8baef36ab1e1f9e6cc41d44159d7527e3
fu1fan <fyf.yf@qq.com>
2021-07-24 21:05:43 +0800

    优化字体渲染

--- modules/theme/default/theme.py
+++ modules/theme/default/theme.py
@@ -3,17 +3,18 @@ import time
 
 from PIL import Image, ImageFont, ImageDraw
 
-import sdk.threadpool_mini
+from sdk import threadpool_mini
 from sdk import display
 
 
 class TextClock(display.Element):
-    def __init__(self, x, y, paper, pool: sdk.threadpool_mini.ThreadPool):
+    def __init__(self, x, y, paper, pool: threadpool_mini.ThreadPool):
         super().__init__(x, y, paper, pool)
         self.last_update = -1
         self.image = Image.new("RGB", (296, 128), 0)
         self.pool = pool
         self.stop_sign = False
+        self.font25 = ImageFont.truetype("resources/fonts/PTSerifCaption.ttc", 54)
 
     def __del__(self):
         self.stop_sign = True
@@ -33,14 +34,13 @@ class TextClock(display.Element):
         now_time = time.strftime("%H : %M", time.localtime())
         now_image = self.image.copy()
         draw_image = ImageDraw.Draw(now_image)
-        font25 = ImageFont.truetype("resources/fonts/PTSerifCaption.ttc", 54)
-        draw_image.text((58, 32), now_time, font=font25)
+        draw_image.text((58, 32), now_time, font=self.font25)
         self.last_update = time.localtime(time.time()).tm_min
         return now_image
 
 
 class Theme:
-    def __init__(self, epd: display.EpdController, pool: sdk.threadpool_mini.ThreadPool):
+    def __init__(self, epd: display.EpdController, pool: threadpool_mini.ThreadPool):
         self.epd = epd
         self.pool = pool
 
