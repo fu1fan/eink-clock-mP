74672ff71034e804b8ceae952416561471b450cb
xuanzhi33 <xuanzhi33@qq.com>
2021-08-05 18:32:40 +0800

    okk

--- sdk/graphics/lib.py
+++ sdk/graphics/lib.py
@@ -14,7 +14,8 @@ class TextElement(Element):
         self.size = size
         self.args = args
         self.kwargs = kwargs
-        self.font = ImageFont.truetype("resources/fonts/STHeiti_Light.ttc", fontSize)
+        self.font = ImageFont.truetype(
+            "resources/fonts/STHeiti_Light.ttc", fontSize)
         self.textColor = textColor
         if bgcolor == "white":
             self.background_image = Image.new("RGB", size, (255, 255, 255))
@@ -35,9 +36,11 @@ class TextElement(Element):
             image_draw = ImageDraw.ImageDraw(image)
             # image_draw.rectangle((0, 0, self.size[0], self.size[1]), fill="white", outline="black", width=1)
             if self.textColor == "black":
-                image_draw.text((5, 5), self.text, font=self.font, fill=(0, 0, 0))
+                image_draw.text((5, 5), self.text,
+                                font=self.font, fill=(0, 0, 0))
             elif self.textColor == "white":
-                image_draw.text((5, 5), self.text, font=self.font, fill=(255, 255, 255))
+                image_draw.text((5, 5), self.text,
+                                font=self.font, fill=(255, 255, 255))
             return image
         elif not self._visible:
             return None
@@ -49,7 +52,8 @@ class TextElement(Element):
 class Button(TextElement):
     def __init__(self, xy, paper: PaperDynamic, text, onclick, size=(60, 33), bgcolor="white", textColor="black",
                  fontSize=20, *args, **kwargs):
-        super().__init__(xy, paper, text, size, bgcolor, textColor, fontSize, *args, **kwargs)
+        super().__init__(xy, paper, text, size, bgcolor,
+                         textColor, fontSize, *args, **kwargs)
         self.on_clicked = onclick
 
     def clickedHandler(self):
@@ -58,9 +62,12 @@ class Button(TextElement):
 
     def init(self):
         super().init()
-        # print("%d  %d, %d  %d" % (self.x,self.y,self.size[0],self.size[1]))
         self.paper.env.touch_handler.add_clicked((self.xy[0], self.xy[0] + self.size[0],
                                                   self.xy[1], self.xy[1] + self.size[1]),
                                                  self.clickedHandler,
                                                  *self.args,
                                                  **self.kwargs)
+
+
+class Label:
+    pass
