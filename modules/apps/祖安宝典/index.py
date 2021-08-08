495eeec23d1e8582645483d037450492cfb028c6
xuanzhi33 <xuanzhi33@qq.com>
2021-08-08 22:31:20 +0800

    祖安宝典app\滑稽

--- modules/apps/祖安宝典/index.py
+++ modules/apps/祖安宝典/index.py
@@ -1,11 +1,18 @@
 import sdk.graphics.paper_lib
+from sdk.graphics import element_lib
+import requests
+import json
 
 
 def build(env):
 
     paper = sdk.graphics.paper_lib.PaperApp(env)
 
-    paper.addElement("mainPage", sdk.graphics.element_lib.Label(
+    paper.addElement("mainPage", element_lib.Label(
         (100, 0), paper, "祖安宝典", (150, 30), bgcolor="black", textColor="white"))
+
+    zuan = requests.get("https://api.shadiao.app/nmsl?level=min").text
+    zuan = json.loads(zuan)["data"]["text"]
+    paper.addElement(0, 30, element_lib.Label((0, 35), paper, zuan, (296, 30)))
 
     return paper
