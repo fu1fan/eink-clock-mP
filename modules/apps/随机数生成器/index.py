4335319fb405f19c1e09f7b65eadc4d06a3e8c9b
xuanzhi33 <xuanzhi33@qq.com>
2021-08-13 08:30:15 +0800

    随机数生成器完工！

--- modules/apps/随机数生成器/index.py
+++ modules/apps/随机数生成器/index.py
@@ -1,12 +1,33 @@
 from sdk.graphics import paper_lib, element_lib
+import random
 
+maxN = 100
 
 def build(env):
 
     paper = paper_lib.PaperApp(env)
 
+
+
     paper.addElement("mainPage", element_lib.Label(
-        (100, 0), paper, "随机数生成器", (150, 30), bgcolor="black", textColor="white"))
-    
+        (80, 0), paper, "随机数生成器", (150, 30), bgcolor="black", textColor="white"))
+    numberLabel = element_lib.Label((115, 35), paper, "0", (100, 50), "black", "white", 50)
+    infoLabel = element_lib.Label((0, 95), paper, "当前范围：1-100", (160, 45), "black", "white")
+
+    paper.addElement("mainPage", numberLabel)
+    paper.addElement("mainPage",infoLabel)
+    def randomNum():
+        numberLabel.setText(str(random.randint(1, maxN)))
+
+    def setMaxNum(maxnum=100):
+        global maxN
+        maxN = maxnum
+        infoLabel.setText("当前范围：1-%d" % maxN)
+
+    paper.addElement("mainPage", element_lib.Button((231, 0), paper, "1-6", setMaxNum, (65,30), bgcolor="white", textColor="black", maxnum=6))
+    paper.addElement("mainPage", element_lib.Button((231, 32), paper, "1-10", setMaxNum, (65,30), bgcolor="white", textColor="black", maxnum=10))
+    paper.addElement("mainPage", element_lib.Button((231, 64), paper, "1-100", setMaxNum, (65,30), bgcolor="white", textColor="black", maxnum=100))
+
+    paper.addElement("mainPage", element_lib.Button((231, 98), paper, "生成", randomNum, (65,30), bgcolor="white", textColor="black"))
 
     return paper
