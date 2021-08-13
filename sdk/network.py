3bafa61bf575ba862992bd411a8f1e0944614d05
fu1fan <fyf.yf@qq.com>
2021-08-14 00:11:48 +0800

    优化工作树

--- sdk/network.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def if_online() -> bool:
-    try:
-        response = requests.get("https://pi.simplebytes.cn/ifonline")
-        response.raise_for_status()
-    except requests.RequestException:
-        return False
-    return True
