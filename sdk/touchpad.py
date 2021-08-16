f31c18a4d2ff83157050f77aeb50877f886ddec6
fu1fan <fyf.yf@qq.com>
2021-08-16 23:45:51 +0800

    添加最高优先级触控列表

--- sdk/touchpad.py
+++ sdk/touchpad.py
@@ -36,6 +36,7 @@ class TouchHandler:
     def __init__(self, env):
         self.pool = env.pool
         self.clicked = []  # 当对象被点击并松开后调用指定函数                      ((x1, x2, y1, y2), func, args, kwargs)
+        self.king_clicked = []
         self.clicked_with_time = []  # 当对象被点击并松开且从未中后调用指定函数并返回触摸时间（float） ((x1, x2, y1, y2), func)
         self.touched = []  # 当对象被按下后调用指定函数，直到松开后再次调用另一指定函数 ((x1, x2, y1, y2), func1, func2, args, kwargs)
         self.slide_x = []  # 当屏幕从指定区域被横向滑动后调用指定函数               ((x1, x2, y1, y2), func, args, kwargs)
@@ -91,6 +92,59 @@ class TouchHandler:
                 break
             time.sleep(0.1)
         self.clicked = content
+        self.signal_1 = False
+        return True
+
+    def add_king_clicked(self, area, func, *args, **kwargs):
+        if area[0] > area[1] or area[2] > area[3] or area[0] < 0 or area[1] > 296 or area[2] < 0 or area[3] > 128:
+            raise ValueError("Area out of range!")
+        self.signal_1 = True
+        while True:
+            if not self.signal_2:
+                break
+            time.sleep(0.1)
+        self.king_clicked.append([area, func, args, kwargs, False])
+        self.signal_1 = False
+
+    def remove_king_clicked(self, func) -> bool:  # 未测试
+        self.signal_1 = True
+        while True:
+            if not self.signal_2:
+                break
+            time.sleep(0.1)
+        counter = 0
+        remove_list = []
+        for i in self.clicked:
+            if i[1] == func:
+                remove_list.append(counter)
+            counter += 1
+        counter = 0
+        if len(remove_list) == 0:
+            self.signal_1 = False
+            return False
+        for i in remove_list:
+            del self.king_clicked[i - counter]
+            counter += 1
+        self.signal_1 = False
+        return True
+
+    def set_king_clicked(self, content: list):
+        self.signal_1 = True
+        while True:
+            if not self.signal_2:
+                break
+            time.sleep(0.1)
+        self.king_clicked = content
+        self.signal_1 = False
+        return True
+
+    def clean_king_clicked(self):
+        self.signal_1 = True
+        while True:
+            if not self.signal_2:
+                break
+            time.sleep(0.1)
+        self.king_clicked = []
         self.signal_1 = False
         return True
 
@@ -359,16 +413,22 @@ class TouchHandler:
                 if i[0][0] <= ICNT_Dev.X[0] <= i[0][1] and i[0][2] <= ICNT_Dev.Y[0] <= i[0][3]:
                     self.pool.add(i[1], *i[3], **i[4])  # 如果被点击，且标记为False，则执行func1
                     i[-1] = True
+                    break
+
+            for i in ReIter(self.king_clicked): # 优先处理
+                if i[0][0] <= ICNT_Dev.X[0] <= i[0][1] and i[0][2] <= ICNT_Dev.Y[0] <= i[0][3]:
+                    i[-1] = True
+                    return
 
             for i in ReIter(self.clicked):
                 if i[0][0] <= ICNT_Dev.X[0] <= i[0][1] and i[0][2] <= ICNT_Dev.Y[0] <= i[0][3]:
                     i[-1] = True
-                    break
+                    return
 
             for i in ReIter(self.clicked_with_time):
                 if i[0][0] <= ICNT_Dev.X[0] <= i[0][1] and i[0][2] <= ICNT_Dev.Y[0] <= i[0][3]:
                     i[-1] = time.time()
-                    break
+                    return
 
             for i in ReIter(self.slide_x):
                 if i[0][0] <= ICNT_Dev.X[0] <= i[0][1] and i[0][2] <= ICNT_Dev.Y[0] <= i[0][3]:
@@ -385,6 +445,12 @@ class TouchHandler:
             for i in ReIter(self.touched):
                 if i[-1]:
                     self.pool.add(i[2], *i[3], **i[4])  # 如果没有被点击，且标记为True，则执行func2
+                    i[-1] = False
+
+            for i in ReIter(self.king_clicked):
+                if i[-1]:
+                    if i[0][0] <= ICNT_Old.X[0] <= i[0][1] and i[0][2] <= ICNT_Old.Y[0] <= i[0][3]:
+                        self.pool.add(i[1], *i[2], **i[3])
                     i[-1] = False
 
             for i in ReIter(self.clicked):
